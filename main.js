4bcf9e4238e8a4011d10dbb0c09c76f7b1a38045
Kemi Thomas <kemothom71@gmail.com>
2020-02-19 21:06:06 -0500

    trying the lazyloader

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-for (let i = 1; i < 800; i++) {
+// let options = {
+//   root: document.querySelector("target"),
+//   rootMargin: "0px, 0px, 100px, 0px"
+// };
+// console.log(options);
+for (let i = 1; i < 100; i++) {
+  // let images = [...document.querySelectorAll(".images")];
+
+  // const callback = entries => {
+  //   entries.forEach(entry => {
+  //     if (entry.isIntersecting) {
+  //       isI;
+  //     }
+  //   });
+  // };
+
+  // images.forEach(image => {
+  //   observer.observe(image);
+  //   }))
+
   fetch(`https://fizal.me/pokeapi/api/v2/id/${i}.json`)
-    .then(function(r){
+    .then(function(r) {
       return r.json();
     })
     .then(function(data) {
